cef6e377ec98ca7902d475e18d4d64e858897723
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-08-03 12:06:15 +0200

    Added error handling.

--- nomad/files.py
+++ nomad/files.py
@@ -19,16 +19,16 @@ file storage system.
 Responsibilities: create, access files; create, receive, notify on, and access uploads.
 """
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
 import shutil
 from minio import Minio
-from minio.error import BucketAlreadyOwnedByYou
+import minio.error
 import logging
 import itertools
 
 import nomad.config as config
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 _client = Minio('%s:%s' % (config.minio.host, config.minio.port),
                 access_key=config.minio.accesskey,
@@ -38,9 +38,9 @@ _client = Minio('%s:%s' % (config.minio.host, config.minio.port),
 # ensure all neccessary buckets exist
 try:
     _client.make_bucket(bucket_name=config.s3.uploads_bucket)
-    logger.info("Created uploads bucket with name %s." % config.s3.uploads_bucket)
-except BucketAlreadyOwnedByYou:
-    logger.debug("Uploads bucket with name %s already existed." % config.s3.uploads_bucket)
+    LOGGER.info("Created uploads bucket with name %s." % config.s3.uploads_bucket)
+except minio.error.BucketAlreadyOwnedByYou:
+    LOGGER.debug("Uploads bucket with name %s already existed." % config.s3.uploads_bucket)
 
 
 def get_presigned_upload_url(upload_id):
@@ -68,18 +68,18 @@ def upload_put_handler(func):
             try:
                 event_name = event_record['eventName']
                 if event_name == 's3:ObjectCreated:Put':
-                    logger.debug('Received bucket upload event of type %s.' % event_name)
+                    LOGGER.debug('Received bucket upload event of type %s.' % event_name)
                     upload_id = event_record['s3']['object']['key']
                     yield upload_id
                 else:
-                    logger.debug('Unhandled bucket event of type %s.' % event_name)
+                    LOGGER.debug('Unhandled bucket event of type %s.' % event_name)
             except KeyError:
-                logger.warning(
+                LOGGER.warning(
                     'Unhandled bucket event due to unexprected event format: %s' %
                     event_record)
 
     def wrapper(*args, **kwargs):
-        logger.info('Start listening to uploads notifications.')
+        LOGGER.info('Start listening to uploads notifications.')
 
         events = _client.listen_bucket_notification(config.s3.uploads_bucket)
 
@@ -93,11 +93,20 @@ def upload_put_handler(func):
                     'Handling of upload notifications was stopped via StopIteration.')
                 return
             except Exception as e:
-                logger.error(
+                LOGGER.error(
                     'Unexpected exception in upload handler for upload:id:' %
                     upload_id, exc_info=e)
 
     return wrapper
+
+
+class UploadError(Exception):
+    IMPLEMENTATION_ERROR = 'implementation error'
+    NOT_ZIP = 'upload is not a zip file'
+
+    def __init__(self, msg, cause, code=IMPLEMENTATION_ERROR):
+        super().__init__(msg, cause)
+        self.code = code
 
 
 class Upload():
@@ -107,16 +116,45 @@ class Upload():
         self.upload_extract_dir = '%s/uploads_extracted/%s' % (config.fs.tmp, upload_id)
         self.filelist = None
 
-    def open(self):
-        _client.fget_object(config.s3.uploads_bucket, self.upload_id, self.upload_file)
-        zipFile = ZipFile(self.upload_file)
-        zipFile.extractall(self.upload_extract_dir)
-        self.filelist = [zipInfo.filename for zipInfo in zipFile.filelist]
-        zipFile.close()
+    # There is not good way to capsule decorators in a class:
+    # https://medium.com/@vadimpushtaev/decorator-inside-python-class-1e74d23107f6
+    class Decorators:
+        @classmethod
+        def log_upload_error(cls, decorated):
+            def wrapper(self, *args, **kwargs):
+                try:
+                    return decorated(self, *args, **kwargs)
+                except Exception as e:
+                    msg = 'Could not %s upload %s.' % (decorated.__name__, self.upload_id)
+                    LOGGER.error(msg, exc_info=e)
+                    raise UploadError(msg, e)
+            return wrapper
 
+    @Decorators.log_upload_error
+    def open(self):
+        try:
+            _client.fget_object(config.s3.uploads_bucket, self.upload_id, self.upload_file)
+        except minio.error.NoSuchKey:
+            raise KeyError(self.upload_id)
+
+        zipFile = None
+        try:
+            zipFile = ZipFile(self.upload_file)
+            zipFile.extractall(self.upload_extract_dir)
+            self.filelist = [zipInfo.filename for zipInfo in zipFile.filelist]
+        except BadZipFile as e:
+            raise UploadError('Upload is not a zip file', e, UploadError.NOT_ZIP)
+        finally:
+            if zipFile is not None:
+                zipFile.close()
+
+    @Decorators.log_upload_error
     def close(self):
-        os.remove(self.upload_file)
-        shutil.rmtree(self.upload_extract_dir)
+        try:
+            os.remove(self.upload_file)
+            shutil.rmtree(self.upload_extract_dir)
+        except FileNotFoundError:
+            raise KeyError(self.upload_id)
 
     def __enter__(self):
         self.open()
@@ -125,5 +163,6 @@ class Upload():
     def __exit__(self, exc_type, exc, exc_tb):
         self.close()
 
+    @Decorators.log_upload_error
     def open_file(self, filename, *args, **kwargs):
         return open('%s/%s' % (self.upload_extract_dir, filename), *args, **kwargs)
