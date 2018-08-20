d65faee4e12013c8cb1158d9f4e4c70f5447e8aa
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-08-20 12:05:06 +0200

    Added patch documentation.

--- nomad/patch.py
+++ nomad/patch.py
@@ -1,6 +1,9 @@
 import systax.analysis.symmetryanalyzer
 
 
+# A patch for the segfault protection of systax (internally uses protection for spglib calls.)
+# We basically disable the protection. The multiprocessing based original protection.
+# somehow interfers with the celery work infrastructure and leads to a deaklock.
 def segfault_protect_patch(f, *args, **kwargs):
     return f(*args, **kwargs)
 
