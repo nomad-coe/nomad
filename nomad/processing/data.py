ea149509af2e68f4ab198bc912bc9881ce29e436
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-09-18 16:42:55 +0200

    Removed and mark quick fixes that are unecessary or bad.

--- nomad/processing/data.py
+++ nomad/processing/data.py
@@ -413,12 +413,15 @@ class Upload(Chord):
     def all_calcs(self, start, end, order_by='mainfile'):
         return Calc.objects(upload_id=self.upload_id)[start:end].order_by(order_by)
 
-    @staticmethod
+@staticmethod
     def repair_all():
         """
         Utitlity function that will look for suspiciously looking conditions in
         all uncompleted downloads. It ain't a perfect world.
         """
+        # TODO this was added as a quick fix to #37.
+        # Even though it might be strictly necessary, there should be a tested backup
+        # solution for it Chords to not work properly due to failed to fail processings
         uploads = Upload.objects(status__in=[PENDING, RUNNING])
         for upload in uploads:
             completed = upload.processed_calcs
