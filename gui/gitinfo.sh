d56a4688444b7b0bfd3e4a5a2db8670da0b1e4f9
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-09-24 17:26:28 +0200

    Minor changes.

--- gui/gitinfo.sh
+++ gui/gitinfo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo \"{ \\\"log\\\": \\\"$(git log -1 --oneline)\\\", \\\"ref\\\": \\\"$(git describe --all)\\\", \\\"version\\\": \\\"$(git describe)\\\" }\"  > src/gitinfo.json
+echo \"{ \\\"log\\\": \\\"$(git log -1 --oneline)\\\", \\\"ref\\\": \\\"$(git describe --all)\\\", \\\"version\\\": \\\"$(git describe)\\\" }\" > src/gitinfo.json
