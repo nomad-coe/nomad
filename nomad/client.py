0938d704e9b154ca46539806ed13dbe4e5e33f90
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-09-27 10:14:54 +0200

    Use click for client and upload command.

--- nomad/client.py
+++ nomad/client.py
@@ -17,15 +17,31 @@ Simple client library for the nomad api that allows to bulk upload files via she
 """
 
 import os.path
+import os
+import sys
 import subprocess
 import shlex
 import time
 import requests
 from requests.auth import HTTPBasicAuth
+import click
 
 api_base = 'http://localhost/nomad/api'
 
 
+def handle_common_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except requests.exceptions.ConnectionError:
+            click.echo(
+                '\nCould not connect to nomad at %s. '
+                'Check connection and host/port options.' % api_base)
+            sys.exit(0)
+    return wrapper
+
+
+@handle_common_errors
 def upload_file(file_path, name=None, offline=False, user='other@gmail.com', pw='nomad'):
     """
     Upload a file to nomad.
@@ -42,7 +58,7 @@ def upload_file(file_path, name=None, offline=False, user='other@gmail.com', pw=
     post_data = dict(name=name)
     if offline:
         post_data.update(dict(local_path=os.path.abspath(file_path)))
-        print('Process offline ' + str(post_data))
+        click.echo('process offline: %s' % file_path)
 
     upload = requests.post('%s/uploads' % api_base, json=post_data, auth=auth).json()
 
@@ -52,7 +68,7 @@ def upload_file(file_path, name=None, offline=False, user='other@gmail.com', pw=
 
         subprocess.call(shlex.split(upload_cmd))
 
-        print('File uploaded')
+        click.echo('uploaded: %s' % file_path)
 
     while True:
         upload = requests.get('%s/uploads/%s' % (api_base, upload['upload_id']), auth=auth).json()
@@ -64,27 +80,71 @@ def upload_file(file_path, name=None, offline=False, user='other@gmail.com', pw=
             total, successes, failures = (
                 calcs_pagination[key] for key in ('total', 'successes', 'failures'))
 
+        ret = '\n' if status in ('SUCCESS', 'FAILURE') else '\r'
+
         print(
-            'status: %s; task: %s; parsing: %d/%d/%d' %
-            (status, upload['current_task'], successes, failures, total))
+            'status: %s; task: %s; parsing: %d/%d/%d                %s' %
+            (status, upload['current_task'], successes, failures, total, ret), end='')
 
         if status in ('SUCCESS', 'FAILURE'):
             break
 
         time.sleep(5)
 
+    if status == 'FAILURE':
+        click.echo('There have been errors:')
+        for error in upload['errors']:
+            click.echo('    %s' % error)
+
+
+def walk_through_files(path, extension='.zip'):
+    """
+    Returns all abs path of all files in a sub tree of the given path that match
+    the given extension.
+
+    Arguments:
+        path (str): the directory
+        extension (str): the extension, incl. '.', e.g. '.zip' (default)
+    """
+
+    for (dirpath, _, filenames) in os.walk(path):
+        for filename in filenames:
+            if filename.endswith(extension):
+                yield os.path.abspath(os.path.join(dirpath, filename))
+
+
+@click.group()
+@click.option('--host', default='localhost', help='The host nomad runs on, default is "localhost".')
+@click.option('--port', default=80, help='the port nomad runs with, default is 80.')
+def cli(host: str, port: int):
+    global api_base
+    api_base = 'http://%s:%d/nomad/api' % (host, port)
+
+
+@cli.command(
+    help='Upload files to nomad. The given path can be a single file or a directory. '
+    'All .zip files in a directory will be uploaded.')
+@click.argument('PATH', type=click.Path(exists=True))
+@click.option(
+    '--name',
+    help='Optional name for the upload of a single file. Will be ignored on directories.')
+@click.option(
+    '--offline', is_flag=True, default=False,
+    help='Upload files "offline": files will not be uploaded, but processed were they are. '
+    'Only works when run on the nomad host.')
+def upload(path, name: str, offline: bool):
+    if os.path.isfile(path):
+        name = name if name is not None else os.path.basename(path)
+        upload_file(path, name, offline)
+
+    elif os.path.isdir(path):
+        for file in walk_through_files(path):
+            name = os.path.basename(file)
+            upload_file(path, name, offline)
+
+    else:
+        sys.exit(0)
+
 
 if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Install dependencies from NOMAD-coe.')
-    parser.add_argument('--offline', help='"upload" and process offline', action='store_true')
-    parser.add_argument('--name', help='"upload" and process offline', type=str, dest='name', default=None)
-    parser.add_argument('--host', help='the api host to use', type=str, dest='host', default='localhost')
-    parser.add_argument('--port', help='the api port to use', type=int, dest='port', default=80)
-    parser.add_argument('file', metavar='FILE', type=str, help='the file to upload and process')
-
-    args = parser.parse_args()
-    name = args.name if args.name is not None else os.path.basename(args.file)
-    api_base = 'http://%s:%d/nomad/api' % (args.host, args.port)
-    upload_file(args.file, name, args.offline)
+    cli()  # pylint: disable=E1120
--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 from setuptools import setup
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
-setup(name='NOMAD',
-  version='0.1.0',
-  description='NOMAD infrastructure python package',
-  packages=['nomad'])
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session='hack')
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs if 'sphinxcontrib.httpdomain' not in str(ir.req)]
+
+setup(
+    name='nomad',
+    version='0.2.0',
+    description='The nomad@FAIR infrastructure python package',
+    py_modules=['nomad'],
+    install_requires=reqs,
+    entry_points='''
+        [console_scripts]
+        nomad=nomad.client:cli
+    ''')
