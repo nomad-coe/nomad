117a52ac6aa066075b76e89173885a9882b2b6e1
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-10-11 15:17:27 +0200

    Override and chunk_size for local repreoduction downloads.

--- nomad/client.py
+++ nomad/client.py
@@ -138,8 +138,12 @@ class CalcProcReproduction:
     This is a class of :class:`UploadFile` the downloaded raw data will be treated as
     an fake 'upload' that only contains the respective calculation data. This allows us
     to locally run processing code that is very similar to the one used on the server.
+
+    Arguments:
+        archive_id: The archive_id of the calculation to locally process.
+        override: Set to true to override any existing local calculation data.
     """
-    def __init__(self, archive_id: str) -> None:
+    def __init__(self, archive_id: str, override: bool = False) -> None:
         self.calc_hash = utils.archive.calc_hash(archive_id)
         self.mainfile = None
         self.parser = None
@@ -148,12 +152,12 @@ class CalcProcReproduction:
         local_path = os.path.join(config.fs.tmp, 'repro_%s.zip' % archive_id)
         if not os.path.exists(os.path.dirname(local_path)):
             os.makedirs(os.path.dirname(local_path))
-        if not os.path.exists(local_path):
-            # download raw if not already downloaded
+        if not os.path.exists(local_path) or override:
+            # download raw if not already downloaded or if override is set
             self.logger.info('Downloading calc.')
             req = requests.get('%s/raw/%s?all=1' % (api_base, archive_id), stream=True)
             with open(local_path, 'wb') as f:
-                for chunk in req.iter_content():
+                for chunk in req.iter_content(chunk_size=1024):
                     f.write(chunk)
         else:
             self.logger.info('Calc already downloaded.')
@@ -234,9 +238,9 @@ class CalcProcReproduction:
 
 
 @click.group()
-@click.option('--host', default='localhost', help='The host nomad runs on, default is "localhost".')
-@click.option('--port', default=80, help='the port nomad runs with, default is 80.')
-@click.option('--verbose', help='sets log level to debug', is_flag=True)
+@click.option('-h', '--host', default='localhost', help='The host nomad runs on, default is "localhost".')
+@click.option('-p', '--port', default=80, help='the port nomad runs with, default is 80.')
+@click.option('-v', '--verbose', help='sets log level to debug', is_flag=True)
 def cli(host: str, port: int, verbose: bool):
     if verbose:
         config.console_log_level = logging.DEBUG
@@ -288,9 +292,13 @@ def reset():
 
 @cli.command(help='Run processing locally.')
 @click.argument('ARCHIVE_ID', nargs=1, required=True, type=str)
-def local(archive_id):
+@click.option(
+    '--override', is_flag=True, default=False,
+    help='Override existing local calculation data.')
+def local(archive_id, **kwargs):
     utils.configure_logging()
-    with CalcProcReproduction(archive_id) as local:
+    utils.get_logger(__name__).info('Using %s' % api_base)
+    with CalcProcReproduction(archive_id, **kwargs) as local:
         backend = local.parse()
         local.normalize_all(parser_backend=backend)
         # backend.write_json(sys.stdout, pretty=True)
