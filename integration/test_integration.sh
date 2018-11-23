e78beae6ea023d1d2db0800f755aadcc33388ce1
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-11-23 15:29:24 +0100

    Added integration test to CI.

--- integration/test_integration.sh
+++ integration/test_integration.sh
@@ -1,11 +1,18 @@
+#!/bin/sh
+set -e
+
+nomad="docker run gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair:test nomad -p 8000 -h api"
+repo_tool="docker run gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair/coe-repotool:latest"
+repo_webservice="docker run gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair/coe-repowebservice:latest"
+
 # import example calculations
-nomad upload --unstage tests/data/proc/examles_vasp.zip
+$nomad upload tests/data/proc/examles_vasp.zip
 
 # create a new index with coe repoTool
-docker run nomad/coe-repotool:latest
+$repo_tool newIndex --indexName=repo_index --indexNameTopics=repo_topics
 
 # start coe repoServer
-docker run nomad/coe-repowebservice:latest
+# docker run nomad/coe-repowebservice:latest
 
 # try to search for new calculations
-curl localhost:8111/...
+# curl localhost:8111/...
