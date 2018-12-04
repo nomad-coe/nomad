fe2a7cb00122974e0201b742ad9d8bcd4b732f76
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-12-04 15:17:43 +0100

    Fixed integration test script.

--- integration/test_integration.sh
+++ integration/test_integration.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-coe_config_args="-e \"REPO_DB_JDBC_URL=jdbc:postgresql://postgres:5432/nomad\" -e \"REPO_ELASTIC_URL=elasticsearch://elastic:9200\""
+coe_config_args="-e REPO_DB_JDBC_URL=jdbc:postgresql://postgres:5432/nomad -e REPO_ELASTIC_URL=elasticsearch://elastic:9200 --network nomad_default"
 
-nomad="docker run gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair:test python -m nomad.client -p 8000 -h api"
-repo_tool="docker $coe_config_args run gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair/coe-repotool:latest"
-repo_webservice="docker $coe_config_args run gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair/coe-repowebservice:latest"
+nomad="nomad -p 8000 -h localhost"
+repo_tool="docker run $coe_config_args gitlab-registry.mpcdf.mpg.de/nomad-lab/nomad-fair/coe-repotool"
 
-# import example calculations
-$nomad upload --unstage /app/tests/data/proc/examples_vasp.zip
+echo "reset nomad"
+$nomad reset
+curl -XDELETE localhost:9200/repo_index
+curl -XDELETE localhost:9200/repo_topics
 
-# create a new index with coe repoTool
+echo "import example calculations"
+$nomad upload --unstage tests/data/proc/examples_vasp.zip
+
+echo "create a new index with coe repoTool"
 $repo_tool newIndex --indexName=repo_index --indexNameTopics=repo_topics
 
-# start coe repoServer
-# docker run nomad/coe-repowebservice:latest
-
 # try to search for new calculations
-# curl localhost:8111/...
+curl http://localhost:8111/repo/search/calculation_groups_oldformat?query=repository_program_name%3DVASP
