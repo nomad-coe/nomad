4a2c2c9b5355116785d63a6e03900ee6ffe01bc9
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-12-17 11:07:21 +0100

    Allow configuration of base paths in gui.

--- gui/src/config.js
+++ gui/src/config.js
@@ -5,11 +5,11 @@ import analytics from '@material-ui/core/colors/lightGreen'
 import secondary from '@material-ui/core/colors/blueGrey'
 import { createMuiTheme } from '@material-ui/core'
 
-export const apiBase = process.env.REACT_APP_API_BASE
-export const objectsBase = process.env.REACT_APP_OBJECT_BASE
-export const appBase = process.env.REACT_APP_APP_BASE
-export const appStaticBase = process.env.REACT_APP_APP_STATIC_BASE
-export const debug = process.env.REACT_APP_DEBUG === 'true'
+window.nomad_env = window.nomad_env || {}
+export const apiBase = process.env.REACT_APP_API_BASE || window.nomad_env.apiBase
+export const appBase = process.env.REACT_APP_APP_BASE || window.nomad_env.appBase
+export const appStaticBase = process.env.REACT_APP_APP_STATIC_BASE || window.nomad_env.appStaticBase
+export const debug = process.env.REACT_APP_DEBUG ? process.env.REACT_APP_DEBUG === 'true' : window.nomad_env.debug
 
 export const genTheme = createMuiTheme({
   palette: {
