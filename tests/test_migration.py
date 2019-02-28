83bebfb575af88f39372ab0e53638ed9d41163e5
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-02-28 15:26:41 +0100

    Fixed order/constrains when preparing migration target db in tests.

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -71,9 +71,9 @@ def source_repo(monkeysession, postgres_infra):
 @pytest.fixture(scope='function')
 def target_repo(postgres):
     with create_postgres_infra(readonly=False, exists=False, dbname=test_target_db_name) as db:
-        db.execute('DELETE FROM affiliations;')
         db.execute('DELETE FROM sessions WHERE user_id >= 3;')
         db.execute('DELETE FROM users WHERE user_id >= 3;')
+        db.execute('DELETE FROM affiliations;')
         assert db.query(coe_repo.User).filter_by(email='admin').first() is not None
         yield db
         db.execute('TRUNCATE uploads CASCADE;')
