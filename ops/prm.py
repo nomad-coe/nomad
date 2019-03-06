e330f097e6bdf40cca60b843928fe62604b9862c
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-03-06 11:26:47 +0100

    Par rm based on system rm.

--- ops/prm.py
+++ ops/prm.py
@@ -1,8 +1,11 @@
+import os
 import sys
 from multiprocessing import Pool
-import shutil
 
 if __name__ == '__main__':
+    def rm(path):
+        os.system('rm -rf %s' % path)
+
     paths = sys.argv[1:]
     with Pool(23) as p:
-        p.map(shutil.rmtree, paths)
+        p.map(rm, paths)
