ab25dc2a79eeb49fc55ec7997e876b5da77964f9
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-03-28 13:12:52 +0100

    Add kubeadm reset script.

--- ops/scripts/kubernetes_install_master.sh
+++ ops/scripts/kubernetes_install_master.sh
@@ -48,6 +48,8 @@ systemctl restart kubelet
 
 # init master node with flannel
 kubeadm init --pod-network-cidr=10.244.0.0/16
+export KUBECONFIG=/etc/kubernetes/admin.conf
+sysctl net.bridge.bridge-nf-call-iptables=1
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/a70459be0084506e4ec919aa1c114638878db11b/Documentation/kube-flannel.yml
 
 # allow to schedule nodes on master
