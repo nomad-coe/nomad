dc3f8f4f2d1b6f850acc3434109fdcdb300cd14c
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-04-01 10:00:52 +0200

    Quickfix to gitinfo generation to fix syntax error.

--- gitinfo.sh
+++ gitinfo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo log: str = \'$(git log -1 --oneline)\'\\nref: str = \'$(git describe --all)\'\\nversion: str = \'$(git describe)\' > nomad/gitinfo.py
+echo log, ref, version = \'$(git log -1 --oneline)\', \'$(git describe --all)\', \'$(git describe)\' > nomad/gitinfo.py
