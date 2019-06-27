8d262cd220ad7e04121d3a5263163afa7160c50a
speckhard <dts@stanford.edu>
2019-06-27 12:38:39 +0200

    removed useless test files.

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -58,8 +58,7 @@ parser_examples = [
     ('parsers/elk', 'tests/data/parsers/elk/Al/INFO.OUT'),
     ('parsers/elastic', 'dependencies/parsers/elastic/test/examples/2nd/INFO_ElaStic'),  # 70Mb file 2big4git
     ('parsers/turbomole', 'tests/data/parsers/turbomole/acrolein.out'),
-    ('parsers/gamess', 'tests/data/parsers/gamess/exam01.out'),
-    ('parsers/vaspoutcar', 'tests/data/parsers/vasp/failed/OUTCAR')
+    ('parsers/gamess', 'tests/data/parsers/gamess/exam01.out')
 ]
 
 # We need to remove some cases with external mainfiles, which might not exist
