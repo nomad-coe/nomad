1edfb52fae9a65fd91bc95806971fecd6ec54e4b
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-07-04 15:27:38 +0200

    Improved deployments for production. [skip ci]

--- ops/deployments/nomad.latest.env.sh
+++
@@ -1,9 +0,0 @@
-export NOMAD_CLIENT_URL=https://labdev-nomad.esc.rzg.mpg.de/fairdi/nomad/latest/api
-export NOMAD_CLIENT_USER=admin
-export NOMAD_MONGO_DB_NAME=fairdi_nomad_latest
-export NOMAD_ELASTIC_INDEX_NAME=fairdi_nomad_latest
-export NOMAD_REPOSITORY_DB_DBNAME=fairdi_nomad_latest
-export NOMAD_FS_LOCAL_TMP=/nomad/fairdi/latest/fs/tmp
-export NOMAD_FS_STAGING=/nomad/fairdi/latest/fs/staging
-export NOMAD_FS_PUBLIC=/nomad/fairdi/latest/fs/public
-export NOMAD_FS_TMP=/nomad/fairdi/latest/fs/tmp
