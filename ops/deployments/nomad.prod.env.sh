3c227a2b7f3d99a575ee910b70d6c20ecfb4db04
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-08-01 10:53:59 +0200

    Added the production deployment.

---
+++ ops/deployments/nomad.prod.env.sh
@@ -0,0 +1,8 @@
+export NOMAD_CLIENT_URL=https://repository.nomad-coe.eu/uploads/api
+export NOMAD_CLIENT_USER=admin
+export NOMAD_MONGO_DB_NAME=fairdi_nomad_prod
+export NOMAD_ELASTIC_INDEX_NAME=fairdi_nomad_prod
+export NOMAD_FS_LOCAL_TMP=/nomad/fairdi/prod/fs/tmp
+export NOMAD_FS_STAGING=/nomad/fairdi/prod/fs/staging
+export NOMAD_FS_PUBLIC=/nomad/fairdi/prod/fs/public
+export NOMAD_FS_TMP=/nomad/fairdi/prod/fs/tmp
