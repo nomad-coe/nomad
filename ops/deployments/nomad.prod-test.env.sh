319be6711277c36a500ff2815fede038898fe5fc
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-08-19 08:36:39 +0200

    Added a deployment that uses the prod data, but is not connected to CoE repo, for tests on the prod data. [skip ci]

---
+++ ops/deployments/nomad.prod-test.env.sh
@@ -0,0 +1,11 @@
+export NOMAD_CLIENT_URL=https://labdev-nomad.esc.rzg.mpg.de/fairdi/nomad/prod-test/api
+export NOMAD_CLIENT_USER=admin
+export NOMAD_MONGO_DB_NAME=fairdi_nomad_prod
+export NOMAD_ELASTIC_INDEX_NAME=fairdi_nomad_prod
+export NOMAD_FS_LOCAL_TMP=/nomad/fairdi/prod/fs/tmp
+export NOMAD_FS_STAGING=/nomad/fairdi/prod/fs/staging
+export NOMAD_FS_PUBLIC=/nomad/fairdi/prod/fs/public
+export NOMAD_FS_TMP=/nomad/fairdi/prod/fs/tmp
+export NOMAD_FS_PREFIX_SIZE=1
+export NOMAD_CELERY_ROUTING=worker
+export NOMAD_REPOSITORY_DB_DBNAME=fairdi_nomad_migration
