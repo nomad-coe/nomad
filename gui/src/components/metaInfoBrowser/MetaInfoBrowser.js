7a69355c4178bbceb6b0c2c5e7fdc7dee4367ace
Markus Scheidgen <markus.scheidgen@gmail.com>
2019-12-19 08:59:37 +0100

    Minor linting.

--- gui/src/components/metaInfoBrowser/MetaInfoBrowser.js
+++ gui/src/components/metaInfoBrowser/MetaInfoBrowser.js
@@ -44,7 +44,6 @@ const ITEM_PADDING_TOP = 8
 const MenuProps = {
   PaperProps: {
     style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
       width: 300, maxHeight: '90vh'
     }
   }
--- gui/src/components/uploads/ConfirmDialog.js
+++ gui/src/components/uploads/ConfirmDialog.js
@@ -5,7 +5,6 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import { FormGroup, Checkbox, FormLabel } from '@material-ui/core'
 import Markdown from '../Markdown'
 
 class ConfirmDialog extends React.Component {
