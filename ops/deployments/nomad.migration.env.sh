3e8dc8a73f11cc611dab395ddae0ca05b1724600
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-02-03 11:07:02 +0100

    Removed old deployments after move to private.

--- ops/deployments/nomad.migration.env.sh
+++
@@ -1,5 +0,0 @@
-export NOMAD_CLIENT_URL=https://labdev-nomad.esc.rzg.mpg.de/fairdi/nomad/migration/api
-export NOMAD_CLIENT_USER=admin
-export NOMAD_FS_MIGRATION_PACKAGES=/nomad/fairdi/migration/fs/migration_packages
-export NOMAD_MONGO_DB_NAME=coe_migration
-export NOMAD_FS_LOCAL_TMP=/nomad/fairdi/migration/fs/tmp
