de98ab6760bf89fcc6f5f373cefd8c9f7711d25f
Lauri Himanen <lauri.himanen@gmail.com>
2020-02-10 18:05:06 +0200

    Updated prototype data to be only specified if a match is found.

--- nomad/normalizing/system.py
+++ nomad/normalizing/system.py
@@ -479,35 +479,28 @@ class SystemNormalizer(SystemBasedNormalizer):
                 if (class_test or comp_test) is False:
                     self.logger.warning('Mismatch in Springer classification or compounds')
 
-    def prototypes(self, atomSpecies, wyckoffs, spg_nr):
-        try:
-            norm_wyckoff = structure.get_normalized_wyckoff(atomSpecies, wyckoffs)
-            protoDict = structure.search_aflow_prototype(spg_nr, norm_wyckoff)
+    def prototypes(self, atom_species: np.array, wyckoffs: np.array, spg_number: int) -> None:
+        """Tries to match the material to an entry in the AFLOW prototype data.
+        If a match is found, a section_prototype is added to section_system.
 
-            if protoDict is None:
-                proto = "%d-_" % spg_nr
-                labels = dict(prototype_label=proto)
-            else:
-                proto = '%d-%s-%s' % (spg_nr, protoDict.get("Prototype", "-"),
-                                      protoDict.get("Pearsons Symbol", "-"))
-                aflow_prototype_id = protoDict.get("aflow_prototype_id", "-")
-                aflow_prototype_url = protoDict.get("aflow_prototype_url", "-")
-                labels = dict(
-                    prototype_label=proto,
-                    prototype_aflow_id=aflow_prototype_id,
-                    prototype_aflow_url=aflow_prototype_url)
-        except Exception as e:
-            self.logger.error("cannot create AFLOW prototype", exc_info=e)
-            return
-
-        pSect = self._backend.openSection("section_prototype")
-        self._backend.addValue(
-            "prototype_assignement_method", "normalized-wyckoff")
-        self._backend.addValue("prototype_label", labels['prototype_label'])
-        aid = labels.get("prototype_aflow_id")
-        if aid:
-            self._backend.addValue("prototype_aflow_id", aid)
-        aurl = labels.get("prototype_aflow_url")
-        if aurl:
-            self._backend.addValue("prototype_aflow_url", aurl)
-        self._backend.closeSection("section_prototype", pSect)
+        Args:
+            atomic_numbers: Array of atomic numbers.
+            wyckoff_letters: Array of Wyckoff letters as strings.
+            spg_number: Space group number.
+        """
+        norm_wyckoff = structure.get_normalized_wyckoff(atom_species, wyckoffs)
+        protoDict = structure.search_aflow_prototype(spg_number, norm_wyckoff)
+        if protoDict is not None:
+            aflow_prototype_id = protoDict["aflow_prototype_id"]
+            aflow_prototype_url = protoDict["aflow_prototype_url"]
+            prototype_label = '%d-%s-%s' % (
+                spg_number,
+                protoDict.get("Prototype", "-"),
+                protoDict.get("Pearsons Symbol", "-")
+            )
+            pSect = self._backend.openSection("section_prototype")
+            self._backend.addValue("prototype_label", prototype_label)
+            self._backend.addValue("prototype_aflow_id", aflow_prototype_id)
+            self._backend.addValue("prototype_aflow_url", aflow_prototype_url)
+            self._backend.addValue("prototype_assignement_method", "normalized-wyckoff")
+            self._backend.closeSection("section_prototype", pSect)
