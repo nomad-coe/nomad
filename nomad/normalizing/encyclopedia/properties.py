50a70fc6683d833e729e3157ae7b6a3398e4a5f9
Lauri Himanen <lauri.himanen@gmail.com>
2020-05-19 16:20:58 +0300

    Fixed linting.

--- nomad/normalizing/encyclopedia/properties.py
+++ nomad/normalizing/encyclopedia/properties.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-
 from nomad.datamodel.encyclopedia import (
     Calculation,
     Properties,
