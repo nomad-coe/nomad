e5a3a25671166e13b6defe421a58544d1fd68a51
Lauri Himanen <lauri.himanen@gmail.com>
2020-06-29 10:31:43 +0300

    Fixed linting.

--- gui/src/components/visualization/StructureViewer.js
+++ gui/src/components/visualization/StructureViewer.js
@@ -1,127 +1,126 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react'
 import PropTypes from 'prop-types'
 
-import { createStyles, makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
 import { Paper } from '@material-ui/core'
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import FullscreenIcon from '@material-ui/icons/Fullscreen';
-import CameraAltIcon from '@material-ui/icons/CameraAlt';
-import ReplayIcon from '@material-ui/icons/Replay';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Checkbox from '@material-ui/core/Checkbox';
-import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import FullscreenIcon from '@material-ui/icons/Fullscreen'
+import CameraAltIcon from '@material-ui/icons/CameraAlt'
+import ReplayIcon from '@material-ui/icons/Replay'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import Checkbox from '@material-ui/core/Checkbox'
+import Box from '@material-ui/core/Box'
 
 export default function StructureViewer(props) {
-
   // States
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [fullscreen, setFullscreen] = useState(false);
-  const [showBonds, setShowBonds] = useState(true);
-  const [showAxis, setShowAxis] = useState(true);
-  const [dimensions, setDimensions] = useState({ width:1, height: 1 });
-  const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const [fullscreen, setFullscreen] = useState(false)
+  const [showBonds, setShowBonds] = useState(true)
+  const [showAxis, setShowAxis] = useState(true)
+  const [dimensions, setDimensions] = useState({ width: 1, height: 1 })
+  const open = Boolean(anchorEl)
 
   // References
-  const viewer = useRef(null);
-  const refViewerCanvas = useRef(null);
-  const refContainer = useRef(null);
+  const viewer = useRef(null)
+  const refViewerCanvas = useRef(null)
+  const refContainer = useRef(null)
 
   // Styles
   const useStyles = makeStyles((theme) => {
-    
     // Calculate sizing
-    let maxSize = 0.9;
-    let width, height;
-    let aspectRatio = dimensions.height/dimensions.width;
-    let windowRatio = window.innerHeight/window.innerWidth;
+    let maxSize = 0.9
+    let width, height
+    let aspectRatio = dimensions.height / dimensions.width
+    let windowRatio = window.innerHeight / window.innerWidth
     if (aspectRatio >= windowRatio) {
-      height = maxSize*window.innerHeight;
-      width = height/aspectRatio;
+      height = maxSize * window.innerHeight
+      width = height / aspectRatio
     } else {
-      width = maxSize*window.innerWidth;
-      height = width*aspectRatio;
+      width = maxSize * window.innerWidth
+      height = width * aspectRatio
     }
 
     return {
-    root: {
-      width: "100%",
-      boxSizing: "border-box",  // This should be a global default?
-    },
-    container: {
-      boxSizing: "border-box",  // This should be a global default?
-      display: "flex",
-      width: "100%",
-      height: "100%",
-      flexDirection: "column",
-      backgroundColor: "white",
-    },
-    fullscreen: {
-      boxSizing: "border-box",  // This should be a global default?
-      padding: theme.spacing(2),
-      position: "fixed",
-      zIndex: 2000,
-      left: "50%",
-      top: "50%",
-      width: width + "px",
-      height: height + "px",
-      transform: "translate(-50%, -50%)",
-    },
-    header: {
-      display: "flex",
-      flexDirection: "row",
-      zIndex: 1,
-    },
-    spacer: {
-      flex: 1,
-    },
-    viewerCanvas: {
-      flex: 1,
-      zIndex: 0,
-      minHeight: 0,  // added min-height: 0 to allow the item to shrink to fit inside the container.
-      marginBottom: theme.spacing(2),
-    },
-    iconButton: {
-      backgroundColor: "white", 
+      root: {
+        width: '100%',
+        boxSizing: 'border-box' // This should be a global default?
+      },
+      container: {
+        boxSizing: 'border-box', // This should be a global default?
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        flexDirection: 'column',
+        backgroundColor: 'white'
+      },
+      fullscreen: {
+        boxSizing: 'border-box', // This should be a global default?
+        padding: theme.spacing(2),
+        position: 'fixed',
+        zIndex: 2000,
+        left: '50%',
+        top: '50%',
+        width: width + 'px',
+        height: height + 'px',
+        transform: 'translate(-50%, -50%)'
+      },
+      header: {
+        display: 'flex',
+        flexDirection: 'row',
+        zIndex: 1
+      },
+      spacer: {
+        flex: 1
+      },
+      viewerCanvas: {
+        flex: 1,
+        zIndex: 0,
+        minHeight: 0, // added min-height: 0 to allow the item to shrink to fit inside the container.
+        marginBottom: theme.spacing(2)
+      },
+      iconButton: {
+        backgroundColor: 'white'
+      }
     }
-  }});
-  const classes = useStyles(props);
+  })
+  const classes = useStyles(props)
 
   // Event handlers
   const handleOpenMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleCloseMenu = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
 
   const handleFullscreen = () => {
-    setFullscreen(!fullscreen);
-  };
+    setFullscreen(!fullscreen)
+  }
 
   const handleScreencapture = () => {
-    viewer.current.takeScreenShot(props.calcId);
-  };
+    viewer.current.takeScreenShot(props.calcId)
+  }
 
   const handleClickAway = (event) => {
     // For some reason clicking the screen capture button will cause the browser
     // to fire a click event during the file download. This case is detected by
     // checking the event target properties.
-    let notDownload = event.target.download === undefined ? true : false;
+    let notDownload = event.target.download === undefined
     if (fullscreen && notDownload) {
-      setFullscreen(false);
+      setFullscreen(false)
     }
-  };
+  }
 
   const handleReset = () => {
-    viewer.current.reset();
-    viewer.current.fitToCanvas();
-    viewer.current.render();
-  };
+    viewer.current.reset()
+    viewer.current.fitToCanvas()
+    viewer.current.render()
+  }
 
   // Used to fetch the initial dimensions of the structure viewer. This fixes
   // the aspect ratio that will be used in fullscreen mode.
@@ -130,93 +129,92 @@ export default function StructureViewer(props) {
       setDimensions({
         width: refContainer.current.offsetWidth,
         height: refContainer.current.offsetHeight
-      });
+      })
     }
-  }, []);
+  }, [])
 
   // Run only on first render to initialize the viewer
   useEffect(() => {
     let options = {
       view: {
-        autoResize: true,       // Automatically fit canvas to the root visualization element
-        autoFit: true,          // Automatically set the zoom to fit the structure into the canvas
-        fitMargin: 0.0,          // Margin between visualization and canvas boundary
+        autoResize: true, // Automatically fit canvas to the root visualization element
+        autoFit: true, // Automatically set the zoom to fit the structure into the canvas
+        fitMargin: 0.0 // Margin between visualization and canvas boundary
       },
       controls: {
-        enableZoom: true,       // Enable zoom with mouse wheel/pinch
-        enablePan: true,        // Enable pan with mouse/touch
-        enableRotate: true,     // Enable rotation
-        zoomLevel: 1,           // Default zoom level
+        enableZoom: true, // Enable zoom with mouse wheel/pinch
+        enablePan: true, // Enable pan with mouse/touch
+        enableRotate: true // Enable rotation
       },
       structure: {
-        showParam: true,        // Show lattice parameters
-        showCopies: true,       // Show periodic copies at cell boundary
-        showShadows: false,      // Enable shadows: requires a bit more from GPU
-        allowRepeat: false,     // 
-        showCell: true,         // Show unit cell wireframe
-        wrap: false,            // Wrap atoms to unit cell
-        createLegend: false,    // Show atom labels
-        showLegend: true,       // Show atom labels
-        showOptions: false,      // Show the options toolbar
-        showBonds: true,        // Show or hide bonds
-        radiusScale: 0.8,       // Scaling factor for atomic radii
-        bondScale: 1.2,         // Scaling factor for the automatic bond detection
-        viewCenter: "COC",      // The rotation and view center position. Valid values are: "COP" (center of position), "COC" center of cell, or a custom position given as array of three cartesian coordinates.
+        showParam: true, // Show lattice parameters
+        showCopies: true, // Show periodic copies at cell boundary
+        showShadows: false, // Enable shadows: requires a bit more from GPU
+        allowRepeat: false, // Allow repeating the structure
+        showCell: true, // Show unit cell wireframe
+        wrap: false, // Wrap atoms to unit cell
+        createLegend: false, // Show atom labels
+        showLegend: true, // Show atom labels
+        showOptions: false, // Show the options toolbar
+        showBonds: true, // Show or hide bonds
+        radiusScale: 0.8, // Scaling factor for atomic radii
+        bondScale: 1.2, // Scaling factor for the automatic bond detection
+        viewCenter: 'COC' // The rotation and view center position. Valid values are: "COP" (center of position), "COC" center of cell, or a custom position given as array of three cartesian coordinates.
       },
       style: {
         backgroundColor: [0xffffff, 1]
       }
-    };
-    viewer.current = new window.matviewer.StructureViewer(refViewerCanvas.current, options);
+    }
+    viewer.current = new window.matviewer.StructureViewer(refViewerCanvas.current, options)
     var bulk = {
-        "atomicNumbers": [11, 17, 11, 17, 11, 17, 11, 17],
-        "cell": [
-            [5.6402, 0.0, 0.0],
-            [0.0, 5.6402, 0.0],
-            [0.0, 0.0, 5.6402]
-        ],
-        "scaledPositions": [
-            [0.0, 0.5, 0.0],
-            [0.0, 0.5, 0.5],
-            [0.0, 0.0, 0.5],
-            [0.0, 0.0, 0.0],
-            [0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.0],
-            [0.5, 0.0, 0.0],
-            [0.5, 0.0, 0.5]
-        ],
-        "primitiveCell": [
-            [0.0, 2.8201, 2.8201],
-            [2.8201, 0.0, 2.8201],
-            [2.8201, 2.8201, 0.0]
-        ],
-        "pbc": [true, true, true],
-        "bonds": [[0, 1]]
-    };
-    viewer.current.load(bulk);
-    viewer.current.resizeCanvasToHostElement();
-    viewer.current.fitToCanvas();
-    viewer.current.saveReset();
-    viewer.current.reset();
-  }, []);
+      'atomicNumbers': [11, 17, 11, 17, 11, 17, 11, 17],
+      'cell': [
+        [5.6402, 0.0, 0.0],
+        [0.0, 5.6402, 0.0],
+        [0.0, 0.0, 5.6402]
+      ],
+      'scaledPositions': [
+        [0.0, 0.5, 0.0],
+        [0.0, 0.5, 0.5],
+        [0.0, 0.0, 0.5],
+        [0.0, 0.0, 0.0],
+        [0.5, 0.5, 0.5],
+        [0.5, 0.5, 0.0],
+        [0.5, 0.0, 0.0],
+        [0.5, 0.0, 0.5]
+      ],
+      'primitiveCell': [
+        [0.0, 2.8201, 2.8201],
+        [2.8201, 0.0, 2.8201],
+        [2.8201, 2.8201, 0.0]
+      ],
+      'pbc': [true, true, true],
+      'bonds': [[0, 1]]
+    }
+    viewer.current.load(bulk)
+    viewer.current.resizeCanvasToHostElement()
+    viewer.current.fitToCanvas()
+    viewer.current.saveReset()
+    viewer.current.reset()
+  }, [])
 
   // These handle the viewer settings
   useEffect(() => {
-    viewer.current.toggleBonds(showBonds);
-  }, [showBonds]);
+    viewer.current.toggleBonds(showBonds)
+  }, [showBonds])
   useEffect(() => {
-    viewer.current.toggleLatticeParameters(showAxis);
-  }, [showAxis]);
+    viewer.current.toggleLatticeParameters(showAxis)
+  }, [showAxis])
 
   // Handles fullscreen mode. Needs to be done before render with
   // useLayoutEffect to avoid stuttering.
   useLayoutEffect(() => {
     if (viewer.current !== null) {
-      viewer.current.resizeCanvasToHostElement();
-      viewer.current.fitToCanvas();
-      viewer.current.render();
+      viewer.current.resizeCanvasToHostElement()
+      viewer.current.fitToCanvas()
+      viewer.current.render()
     }
-  }, [fullscreen]);
+  }, [fullscreen])
 
   return (
     <Box className={classes.root}>
@@ -238,37 +236,37 @@ export default function StructureViewer(props) {
             </IconButton>
           </div>
           <Menu
-            id="settings-menu"
+            id='settings-menu'
             anchorEl={anchorEl}
             getContentAnchorEl={null}
-            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-            transformOrigin={{ vertical: "top", horizontal: "left" }}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'left' }}
             keepMounted
             open={open}
             onClose={handleCloseMenu}
           >
-            <MenuItem key="show-bonds">
+            <MenuItem key='show-bonds'>
               <FormControlLabel
                 control={
-                <Checkbox
-                  checked={showBonds}
-                  onChange={(event) => {setShowBonds(!showBonds)}}
-                  color="primary"
-                />
+                  <Checkbox
+                    checked={showBonds}
+                    onChange={(event) => { setShowBonds(!showBonds) }}
+                    color='primary'
+                  />
                 }
-                label="Show bonds"
+                label='Show bonds'
               />
             </MenuItem>
-            <MenuItem key="show-axis">
+            <MenuItem key='show-axis'>
               <FormControlLabel
                 control={
-                <Checkbox
-                  checked={showAxis}
-                  onChange={(event) => {setShowAxis(!showAxis)}}
-                  color="primary"
-                />
+                  <Checkbox
+                    checked={showAxis}
+                    onChange={(event) => { setShowAxis(!showAxis) }}
+                    color='primary'
+                  />
                 }
-                label="Show axis"
+                label='Show axis'
               />
             </MenuItem>
           </Menu>
@@ -280,5 +278,6 @@ export default function StructureViewer(props) {
 }
 
 StructureViewer.propTypes = {
-  info: PropTypes.object
+  info: PropTypes.object,
+  calcId: PropTypes.string
 }
