Fixed normalization issues in parsers, introduced dos energy normalization in the DOSNormalizer. It will try to find the HOMO energy and if successfull use it as reference. Otherwise it will default to the Fermi energy as reported by the code.

nomad/normalizing/dos.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 
 import numpy as np
+import bisect
 
+from nomad import config, units
 from nomad_dos_fingerprints import DOSFingerprint
-
 from nomad.datamodel.metainfo.public import section_dos_fingerprint
 from nomad.atomutils import get_volume
 
@@ -45,15 +46,15 @@ class DosNormalizer(Normalizer):
 
             for dos in section_dos:
                 dos_values = dos.dos_values
-                if dos_values is None:
-                    # section dos without dos_values
+                dos_energies = dos.dos_energies
+                if dos_energies is None or dos_values is None:
                     continue
 
+                # Normalize DOS values to be 1/J/atom/m^3
                 system = scc.single_configuration_calculation_to_system_ref
                 if system is None:
                     self.logger.error('referenced system for dos calculation could not be found')
                     continue
-
                 atom_positions = system.atom_positions
                 lattice_vectors = system.lattice_vectors
                 if atom_positions is None:
@@ -62,25 +63,76 @@ class DosNormalizer(Normalizer):
                 if lattice_vectors is None:
                     self.logger.error('required quantity lattice_vectors is not available')
                     return
-
                 number_of_atoms = np.shape(atom_positions)[0]
                 unit_cell_volume = get_volume(lattice_vectors.magnitude)
+                dos_values_normalized = dos_values / (number_of_atoms * unit_cell_volume)
 
-                # Final quantities
-                dos_normed = dos_values / (number_of_atoms * unit_cell_volume)
+                # Normalize energies so that they are normalized to HOMO when
+                # it can be reliably detected and to Fermi level if HOMO could
+                # not be detected.
+                energy_reference_fermi = scc.energy_reference_fermi[0]
+                fermi_idx = (np.abs(dos_energies - energy_reference_fermi)).argmin()
+                energy_threshold = config.normalize.band_structure_energy_tolerance
+                value_threshold = 1e-8  # The DOS value that is considered to be zero
+                homo_found = False
+                zero_found = False
+                i_homo = 0
+                energy_distance = 0
+
+                # Walk through the energies in descencing direction to see if a
+                # gap is nearby (see energy_threshold). If gap found, continue
+                # until HOMO found
+                while not homo_found:
+                    try:
+                        value = dos_values_normalized[0, fermi_idx + i_homo]
+                        energy_distance = energy_reference_fermi - dos_energies[fermi_idx + i_homo]
+                    except IndexError:
+                        break
+                    if energy_distance.magnitude > energy_threshold and not zero_found:
+                        break
+                    if value <= value_threshold:
+                        zero_found = True
+                    if zero_found and value > 0:
+                        homo_found = True
+                        i_homo += 1
+                        break
+                    i_homo -= 1
+                # If gap was not found in descending direction, check the
+                # ascending direction for a nearby (see energy_threshold) HOMO
+                # value
+                if not homo_found:
+                    i_homo = 1
+                    while not homo_found:
+                        try:
+                            value = dos_values_normalized[0, fermi_idx + i_homo]
+                            energy_distance = dos_energies[fermi_idx + i_homo] - energy_reference_fermi
+                        except IndexError:
+                            break
+                        if energy_distance.magnitude > energy_threshold:
+                            break
+                        if value <= value_threshold:
+                            homo_found = True
+                            break
+                        i_homo += 1
+                if homo_found:
+                    energy_reference = dos_energies[fermi_idx + i_homo]
+                else:
+                    energy_reference = energy_reference_fermi
+                dos_energies_normalized = dos_energies - energy_reference
 
                 # Data for DOS fingerprint
                 dos_fingerprint = None
                 try:
                     dos_energies = dos.dos_energies_normalized
-                    dos_fingerprint = DOSFingerprint().calculate(np.array(dos_energies), dos_normed, n_atoms = number_of_atoms)
+                    dos_fingerprint = DOSFingerprint().calculate(np.array(dos_energies), dos_normed, n_atoms=number_of_atoms)
                 except Exception as e:
                     self.logger.error('could not generate dos fingerprint', exc_info=e)
 
                 # Add quantities to NOMAD's Metainfo
                 scc_url = '/section_run/0/section_single_configuration_calculation/%d/section_dos/0' % scc.m_parent_index
                 self._backend.openContext(scc_url)
-                dos.dos_values_normalized = dos_normed
+                dos.dos_values_normalized = dos_values_normalized
+                dos.dos_energies_normalized = dos_energies_normalized
                 if dos_fingerprint is not None:
                     sec_dos_fingerprint = dos.m_create(section_dos_fingerprint)
                     sec_dos_fingerprint.bins = dos_fingerprint.bins