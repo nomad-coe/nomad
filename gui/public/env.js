e06c82b15e67112c673fc954186fd9d84734b598
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-10-02 13:49:42 +0200

    Updated version number.

--- gui/public/env.js
+++ gui/public/env.js
@@ -9,7 +9,7 @@ window.nomadEnv = {
   'matomoUrl': 'https://nomad-lab.eu/fairdi/stat',
   'matomoSiteId': '2',
   'version': {
-    'label': '0.9.1',
+    'label': '0.9.2',
     'isBeta': false,
     'isTest': true,
     'usesBetaData': true,
