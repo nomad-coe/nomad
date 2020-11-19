6f3cb4810e63bd41d90ca5d5339775f44b331398
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-11-19 09:00:36 +0100

    Small fixes to run.sh.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-find gui/build -type f | xargs -L1 bash -c 'sed "s_/fairdi/nomad/latest/gui_$1/gui_g" $2 > /tmp/temp_file; cp /tmp/temp_file $2;' -- $1
+find gui/build -type f -not -name "env.js" | xargs -L1 bash -c 'sed "s_/fairdi/nomad/latest/gui_$1/gui_g" $2 > /tmp/temp_file; cp /tmp/temp_file $2;' -- $1
 params=()
 [ -e gunicorn.conf ] && params+=(--config gunicorn.conf)
-[ -e gunicorn.log.conf ] && params+=(--log-config, gunicorn.log.conf)
+[ -e gunicorn.log.conf ] && params+=(--log-config gunicorn.log.conf)
 python -m gunicorn.app.wsgiapp "${params[@]}" -b 0.0.0.0:8000 nomad.app:app
