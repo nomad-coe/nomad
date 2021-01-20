7f00e2fb9b4a0023bd01f5479800f8c55b57ecca
Markus Scheidgen <markus.scheidgen@gmail.com>
2021-01-20 17:34:37 +0100

    Added a hash of the exception stacktrace to logstash entries. #479

--- nomad/utils/structlogging.py
+++ nomad/utils/structlogging.py
@@ -47,7 +47,7 @@ from contextlib import contextmanager
 import json
 import re
 
-from nomad import config
+from nomad import config, utils
 
 
 def sanitize_logevent(event: str) -> str:
@@ -151,7 +151,12 @@ class LogstashFormatter(logstash.formatter.LogstashFormatterBase):
                 if key in ('event', 'stack_info', 'id', 'timestamp'):
                     continue
                 elif key == 'exception':
+                    exception_trace = value.strip('\n')
                     message['digest'] = str(value)[-256:]
+                    # exclude the last line, which is the exception message and might
+                    # vary for different instances of the same exception
+                    message['exception_hash'] = utils.hash(
+                        exception_trace[:exception_trace.rfind('\n')])
                 elif key in ['upload_id', 'calc_id', 'mainfile']:
                     key = 'nomad.%s' % key
                 else:
