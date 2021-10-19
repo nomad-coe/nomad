acbe76d1ae91a880d968cc3cc4654945992cc93f
mohammad <mohammad.nakhaee.1@gmail.com>
2021-10-19 22:12:14 +0200

    Set a larger maximum width for browser tabs

--- gui/src/components/archive/Browser.js
+++ gui/src/components/archive/Browser.js
@@ -280,7 +280,7 @@ List.propTypes = ({
 })
 
 export function Content({children}) {
-  return <Box padding={1} style={{maxWidth: 340}}>
+  return <Box padding={1} style={{maxWidth: 1024}}>
     {children}
   </Box>
 }
