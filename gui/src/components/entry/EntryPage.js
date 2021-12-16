d255d5302ca28cde6dabb413c624db5562d464f9
Markus Scheidgen <markus.scheidgen@gmail.com>
2021-12-16 15:31:21 +0100

    Fixed JS error on entrypage archive navigation.

--- gui/src/components/entry/EntryPage.js
+++ gui/src/components/entry/EntryPage.js
@@ -80,7 +80,7 @@ const EntryPage = React.memo(() => {
     <CacheSwitch>
       <CacheRoute path={`${path}`} exact render={() => <OverviewView entryId={entryId}/>} />
       <CacheRoute path={`${path}/raw`} render={() => <RawFileView entryId={entryId}/>} />
-      <CacheRoute path={`${path}/archive`} render={() => <ArchiveEntryView entryId={entryId}/>} />
+      <CacheRoute when="back" path={`${path}/archive`} render={() => <ArchiveEntryView entryId={entryId}/>} />
       <CacheRoute path={`${path}/logs`} render={() => <ArchiveLogView entryId={entryId}/>} />
       <Redirect strict from={`${path}/overview`} to={`${path}`} />
     </CacheSwitch>
