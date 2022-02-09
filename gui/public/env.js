c7fb02b2c03b894b806b03bdb521fa6cc7f3688b
Markus Scheidgen <markus.scheidgen@gmail.com>
2022-02-09 11:06:31 +0100

    Updated version numbers.

--- gui/public/env.js
+++ gui/public/env.js
@@ -11,7 +11,7 @@ window.nomadEnv = {
   'encyclopediaBase': 'https://nomad-lab.eu/prod/rae/encyclopedia/#',
   'debug': false,
   'version': {
-    'label': '1.0.2',
+    'label': '1.0.3',
     'isBeta': false,
     'isTest': true,
     'usesBetaData': true,
--- nomad/config.py
+++ nomad/config.py
@@ -307,7 +307,7 @@ datacite = NomadConfig(
 )
 
 meta = NomadConfig(
-    version='1.0.2',
+    version='1.0.3',
     commit=gitinfo.commit,
     deployment='devel',
     label=None,
