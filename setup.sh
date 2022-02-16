21520b3d203e7d5a6de25ff5edfef4e7e783f502
sanbrock <sandor.brockhauser@gmail.com>
2022-02-16 18:25:01 +0100

    avoid dependency collision

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ pip install --upgrade pip
 git submodule sync --recursive
 sleep 5
 
+# Install nomad
+pip install -e .[all]
+
 # Install sub-modules
 git submodule update --init --recursive --jobs=4
 ./dependencies.sh -e
-
-# Install nomad
-pip install -e .[all]
 
 ./generate_gui_artifacts.sh
