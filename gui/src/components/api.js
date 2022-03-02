b8a864c3f57cb852ad79982ccede804fdc2eb2c2
Markus Scheidgen <markus.scheidgen@gmail.com>
2022-03-02 11:55:31 +0100

    Fixed missing loading indicator.

--- gui/src/components/api.js
+++ gui/src/components/api.js
@@ -270,9 +270,7 @@ class Api {
 
   async doHttpRequest(method, path, body, config) {
     const noLoading = config?.noLoading
-    if (!noLoading) {
-      this.onStartLoading()
-    }
+    this.onStartLoading(!noLoading)
     const auth = await this.authHeaders()
     config = config || {}
     config.params = config.params || {}
@@ -300,9 +298,7 @@ class Api {
       }
       handleApiError(errors)
     } finally {
-      if (!noLoading) {
-        this.onFinishLoading()
-      }
+      this.onFinishLoading(!noLoading)
     }
   }
 
