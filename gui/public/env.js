369632d23527b8df66fe0679e538cb1e13b50356
Markus Scheidgen <markus.scheidgen@gmail.com>
2022-05-05 10:47:01 +0000

    Fixed accidentally committed env.js.

--- gui/public/env.js
+++ gui/public/env.js
@@ -5,10 +5,8 @@ window.nomadEnv = {
   // 'keycloakClientId': 'nomad_public',
   // 'appBase': 'https://nomad-lab.eu/prod/v1',
   // Use the local API
-  // 'keycloakRealm': 'fairdi_nomad_test',
-  // 'keycloakClientId': 'nomad_gui_dev',
-  'keycloakRealm': 'fairdi_nomad_prod',
-  'keycloakClientId': 'nomad_public',
+  'keycloakRealm': 'fairdi_nomad_test',
+  'keycloakClientId': 'nomad_gui_dev',
   'appBase': 'http://localhost:8000/fairdi/nomad/latest',
   'encyclopediaBase': 'https://nomad-lab.eu/prod/rae/encyclopedia/#',
   'northBase': 'http://localhost:9000/fairdi/nomad/latest/north',
