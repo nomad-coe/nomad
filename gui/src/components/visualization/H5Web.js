3509f17fb42b426c04e37fe7c0dea80165968d32
David Sikter <david.sikter@physik.hu-berlin.de>
2022-06-23 10:34:34 +0200

    Upgrading h5web

--- gui/src/components/visualization/H5Web.js
+++ gui/src/components/visualization/H5Web.js
@@ -1,14 +1,13 @@
-// import React from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-// import { appBase } from '../../config'
-// import { App, H5GroveProvider } from '@h5web/app'
-// import { useApi } from '../api'
+import { appBase } from '../../config'
+import { App, H5GroveProvider } from '@h5web/app'
+import { useApi } from '../api'
 
-import '@h5web/app/dist/style-lib.css'
-import '@h5web/app/dist/style.css'
+import '@h5web/lib/dist/styles.css'
+import '@h5web/app/dist/styles.css'
 
 const H5Web = ({upload_id, filename}) => {
-  return null /*
   const {api} = useApi()
   const filepath = upload_id.substring(0, 2) + '/' + upload_id + '/raw/' + filename
   const authParams = {}
@@ -23,7 +22,7 @@ const H5Web = ({upload_id, filename}) => {
     >
       <App />
     </H5GroveProvider>
-  ) */
+  )
 }
 H5Web.propTypes = {
   upload_id: PropTypes.string.isRequired,
