3cfa13a6630e9b99d2252ab06299636cf852fca1
Sherjeel Shabih <shabihsherjeel@gmail.com>
2022-06-27 12:59:03 +0200

    Added support for nexusparser parameter files in h5web

--- gui/src/components/visualization/H5Web.js
+++ gui/src/components/visualization/H5Web.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { appBase } from '../../config'
 import { App, H5GroveProvider } from '@h5web/app'
 import { useApi } from '../api'
+import { useErrors } from '../errors'
 
 import '@h5web/app/dist/style-lib.css'
 import '@h5web/app/dist/style.css'
 
-const H5Web = ({upload_id, filename}) => {
+const H5Web = (props) => {
   const {api} = useApi()
-  const filepath = upload_id.substring(0, 2) + '/' + upload_id + '/raw/' + filename
+  const {raiseError} = useErrors()
+  const [filepath, setFilepath] = useState(false)
+  const {upload_id, filename} = props
   const authParams = {}
   if (api.keycloak) {
     authParams.token = api.keycloak.token
   }
-  return (
-    <H5GroveProvider
-      url={appBase + '/h5grove/'}
-      filepath={filepath}
-      axiosParams={{file: filepath, upload_id: upload_id, ...authParams}}
-    >
-      <App />
-    </H5GroveProvider>
-  )
+
+  useEffect(() => {
+    if (filename.includes('.yaml') || filename.includes('.yml')) {
+      api.get(`/uploads/${upload_id}/raw/${filename}`)
+        .then(data => {
+          let fname = data.match(/(?<=output:\s*)[^\s].*nxs/g)[0]
+          setFilepath(upload_id.substring(0, 2) + '/' + upload_id + '/raw/' + fname)
+        })
+        .catch(raiseError)
+    } else {
+      setFilepath(upload_id.substring(0, 2) + '/' + upload_id + '/raw/' + filename)
+    }
+  }, [filepath, api, filename, upload_id, raiseError])
+
+  if (filepath) {
+    return (
+      <H5GroveProvider
+        url={appBase + '/h5grove/'}
+        filepath={filepath}
+        axiosParams={{file: filepath, upload_id: upload_id, ...authParams}}
+      >
+        <App />
+      </H5GroveProvider>
+    )
+  }
+  return 'Loading...'
 }
 H5Web.propTypes = {
   upload_id: PropTypes.string.isRequired,
