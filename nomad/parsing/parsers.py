698f971c255dd90c8f8b8908ec0967aac226cee2
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2022-08-08 22:36:01 +0200

    Fix commit

--- nomad/parsing/parsers.py
+++ nomad/parsing/parsers.py
@@ -568,8 +568,8 @@ parsers = [
         mainfile_contents_re=(r'^LOBSTER\s*v[\d\.]+.*'),
     ),
     MatchingParserInterface(
-        'workflowparsers.MPParser',
-        name='parsers/mp', code_name='MaterialsProject',
+        'workflowparsers.AtomateParser',
+        name='parsers/atomate', code_name='Atomate',
         code_homepage='https://materialsproject.org',
         mainfile_mime_re=r'(application/json)|(text/.*)',
         mainfile_name_re=r'.*mp.+materials\.json',
