c723d637e48586f15fbe4a4c38109a214df0350c
jrudz <rudzinski@mpip-mainz.mpg.de>
2023-10-31 14:45:09 +0100

    correct if bonds in atomutils

--- nomad/atomutils.py
+++ nomad/atomutils.py
@@ -1261,7 +1261,7 @@ def archive_to_universe(archive, system_index: int = 0, method_index: int = -1, 
     # get the bonds  # TODO extend to multiple storage options for interactions
     bonds = []
     bonds = sec_atoms.bond_list
-    if not bonds:
+    if bonds is not []:
         bonds = get_bond_list_from_model_contributions(sec_run, method_index=-1, model_index=-1)
 
     # get the system times
