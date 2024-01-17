16ca52176fa2f000ec1986517b8241218be56814
Sascha Klawohn <sascha.klawohn@physik.hu-berlin.de>
2024-01-17 10:20:57 +0100

    Refactor: test_get_group_uploads

--- tests/app/v1/routers/uploads/test_group_uploads.py
+++ tests/app/v1/routers/uploads/test_group_uploads.py
@@ -7,65 +7,53 @@ from .common import assert_upload
 
 
 @pytest.mark.parametrize(
-    'kwargs',
+    'user, query_params, expected_upload_ids, expected_status_code',
     [
+        pytest.param(None, {}, [], 401, id='group-guest-user'),
+        pytest.param('invalid', {}, [], 401, id='groups-invalid-user'),
         pytest.param(
-            dict(
-                user=None,
-                expected_upload_ids=[],
-                expected_status_code=401,
-            ),
-            id='groups-guest',
-        ),
-        pytest.param(
-            dict(
-                user='invalid',
-                expected_upload_ids=[],
-                expected_status_code=401,
-            ),
-            id='groups-invalid-user',
-        ),
-        pytest.param(
-            dict(
-                user='other_test_user',
-                expected_upload_ids=[
-                    'id_coauthor_other_group',
-                    'id_reviewer_other_group',
-                    'id_coauthor_mixed_group',
-                    'id_reviewer_mixed_group',
-                ],
-            ),
+            'other_test_user',
+            {},
+            [
+                'id_coauthor_other_group',
+                'id_reviewer_other_group',
+                'id_coauthor_mixed_group',
+                'id_reviewer_mixed_group',
+            ],
+            200,
             id='groups-no-args',
         ),
         pytest.param(
-            dict(
-                user='other_test_user',
-                query_params={'roles': 'coauthor'},
-                expected_upload_ids=[
-                    'id_coauthor_other_group',
-                    'id_coauthor_mixed_group',
-                ],
-            ),
+            'other_test_user',
+            {'roles': 'coauthor'},
+            [
+                'id_coauthor_other_group',
+                'id_coauthor_mixed_group',
+            ],
+            200,
             id='groups-coauthor',
         ),
         pytest.param(
-            dict(
-                user='other_test_user',
-                query_params={'roles': 'reviewer'},
-                expected_upload_ids=[
-                    'id_reviewer_other_group',
-                    'id_reviewer_mixed_group',
-                ],
-            ),
+            'other_test_user',
+            {'roles': 'reviewer'},
+            [
+                'id_reviewer_other_group',
+                'id_reviewer_mixed_group',
+            ],
+            200,
             id='groups-reviewer',
         ),
     ],
 )
-def test_get_group_uploads(client, example_data_groups, test_auth_dict, kwargs):
-    user = kwargs.get('user', 'test_user')
-    query_params = kwargs.get('query_params', {})
-    expected_status_code = kwargs.get('expected_status_code', 200)
-    expected_upload_ids = kwargs.get('expected_upload_ids', None)
+def test_get_group_uploads(
+    client,
+    example_data_groups,
+    test_auth_dict,
+    user,
+    query_params,
+    expected_upload_ids,
+    expected_status_code,
+):
     user_auth, __token = test_auth_dict[user]
 
     response = perform_get(client, 'uploads', user_auth=user_auth, **query_params)
@@ -76,14 +64,9 @@ def test_get_group_uploads(client, example_data_groups, test_auth_dict, kwargs):
     response_json = response.json()
     response_data = response_json['data']
 
-    if expected_upload_ids is not None:
-        assert (
-            len(response_data) == len(expected_upload_ids)
-        ), f'Wrong number of records returned, expected {len(expected_upload_ids)}, got {len(response_data)}'
-        found_upload_ids = [upload['upload_id'] for upload in response_data]
-        assert (
-            expected_upload_ids == found_upload_ids
-        ), f'Wrong upload is list returned. Expected {repr(expected_upload_ids)}, got {repr(found_upload_ids)}.'
+    assert len(response_data) == len(expected_upload_ids)
+    found_upload_ids = [upload['upload_id'] for upload in response_data]
+    assert expected_upload_ids == found_upload_ids
 
 
 @pytest.mark.parametrize(
