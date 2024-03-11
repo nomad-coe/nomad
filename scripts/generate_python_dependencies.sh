ae5e3d83c724565c72305bdd980bd8fba0a96520
Ahmed Ilyas <ahmed.ilyas@hu-berlin.de>
2024-03-11 12:36:28 +0000

    Remove submodules from dev dependencies
    Changelog: Changed

--- scripts/generate_python_dependencies.sh
+++ scripts/generate_python_dependencies.sh
@@ -23,11 +23,5 @@ pip-compile --resolver=backtracking --annotation-style=line \
     --extra=dev --extra=infrastructure --extra=parsing \
     --output-file=requirements-dev.txt \
     requirements.txt \
-    dependencies/nomad-dos-fingerprints/pyproject.toml \
-    dependencies/parsers/atomistic/pyproject.toml \
-    dependencies/parsers/database/pyproject.toml \
-    dependencies/parsers/eelsdb/pyproject.toml \
-    dependencies/parsers/electronic/pyproject.toml \
-    dependencies/parsers/nexus/pyproject.toml \
-    dependencies/parsers/workflow/pyproject.toml \
     pyproject.toml
+
