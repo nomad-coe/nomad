f0b4279643104c007bc93ea0cd881f9ea3f90fc2
Haoyu Yang <yanghaoyu97@outlook.com>
2026-07-02 15:23:09 +0200

    Rename upload tests

--- tests/fixtures/infrastructure.py
+++ tests/fixtures/infrastructure.py
@@ -74,7 +74,7 @@ class CustomScheduler(LoadScopeScheduling):
     integration_tests = [
         'tests/app/v1/routers/test_apps.py',
         'tests/app/v1/routers/test_federation.py',
-        'tests/app/v1/routers/uploads/test_basic_uploads_legacy.py',
+        'tests/app/v1/routers/uploads/test_uploads_non_xdist.py',
         'tests/app/v1/routers/uploads/test_transfer_bundle.py',
         'tests/archive/test_archive.py',
         'tests/logtransfer/test_logtransfer.py',
